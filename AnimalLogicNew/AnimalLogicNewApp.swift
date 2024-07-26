fbb0e55f43e291e13aad6480ea0dd599426fe91a
william farhang <farhang.william@gmail.com>
2024-07-26 19:31:44 +0200

    Initial Commit

---
+++ AnimalLogicNew/AnimalLogicNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalLogicNewApp.swift
+//  AnimalLogicNew
+//
+//  Created by william farhang on 2024-07-26.
+//
+
+import SwiftUI
+
+@main
+struct AnimalLogicNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalLogicNew/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimalLogicNew
+//
+//  Created by william farhang on 2024-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
